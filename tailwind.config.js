e98784ad3016ed0eb3dd2d19185bdc187f7d943b
jungle379 <jungle379@gmail.com>
2021-10-07 21:42:33 +0900

    🐛

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const tailwindMobile = require('tailwind-mobile/config');
 
 // wrap config with tailwindMobile config
 module.exports = tailwindMobile({
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
