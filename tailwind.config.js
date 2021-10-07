edb8cd8fbaca4f404a501294490f2dff85d2856c
jungle379 <jungle379@gmail.com>
2021-10-08 05:40:33 +0900

    🎨

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 // import tailwindMobile config
-const tailwindMobile = require('tailwind-mobile/config');
+import tailwindMobile from 'tailwind-mobile/config';
 
 // wrap config with tailwindMobile config
-module.exports = tailwindMobile({
+export default tailwindMobile({
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
